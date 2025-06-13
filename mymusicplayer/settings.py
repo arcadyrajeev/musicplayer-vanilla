a72a9239cf8cfc4c2ee28059022a621063db050b
Rajeev <bitmasteratj@gmail.com>
2025-06-13 21:12:29 +0530

    settings change

--- mymusicplayer/settings.py
+++ mymusicplayer/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,7 +125,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'mainapp/static')]
+STATICFILES_DIRS = [BASE_DIR / 'mainapp' / 'static']
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # For collectstatic command
 
 # Media files (User uploads)
@@ -136,3 +138,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # For user-uploaded files
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
