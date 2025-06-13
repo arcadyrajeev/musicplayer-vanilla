3528f46e5ec4b9cdd36da6b50ea979fb89ffabb6
Rajeev <bitmasteratj@gmail.com>
2025-06-13 20:57:50 +0530

    new

--- mymusicplayer/urls.py
+++ mymusicplayer/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
+     path('', RedirectView.as_view(url='/mainapp/')),
     path("mainapp/", include("mainapp.urls")),
     path("admin/", admin.site.urls),
 ]
